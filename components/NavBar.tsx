f393b9386f129b30ab859a945dfaf238a98a03fe
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-08 19:46:50 +0545

    docs: navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -9,7 +9,37 @@ const NavBar = () => {
     <header className="navbar">
       <nav>
         <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="Logo Image"
+            width={32}
+            height={32}
+          ></Image>
+          <h1>SnapCast</h1>
         </Link>
+
+        {user && (
+          <figure>
+            <button>
+              <Image
+                src="/assets/images/dummy.jpg"
+                width={36}
+                height={36}
+                alt="User Profile"
+                className="rounded-full aspect-square"
+              ></Image>
+            </button>
+            <button className="cursor-pointer">
+              <Image
+                src="/assets/icons/logout.svg"
+                width={24}
+                height={24}
+                className="rotate-180"
+                alt="Logout"
+              ></Image>
+            </button>
+          </figure>
+        )}
       </nav>
     </header>
   );
