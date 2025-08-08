24e26dc112412ec759013c3d19cf118b6c12f16d
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-08 21:32:45 +0545

    docs: navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Page = () => {
   return (
     <main className="wrapper page">
-      <h1 className="text-2xl font-karla">Welcome to the ScreenCast</h1>
+      <h1 className="text-2xl font-karla">Welcome to Loom Clone</h1>
     </main>
   );
 };
