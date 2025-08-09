87a5c10d8a71afeaa6982b38b45adef156d0d00c
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-09 13:11:20 +0545

    docs: cont.

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,7 +1,20 @@
+import Header from "@/components/Header";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const page = async ({ params }: ParamsWithSearch) => {
+  const { id } = await params;
+  return (
+    <div>
+      {/* Reuse the Header Component */}
+      <Header
+        subHeader="birat@gmai.com"
+        title="Birat | Geek Coder"
+        userImg="/assets/images/dummy.jpg"
+      ></Header>
+      USER ID : {id}
+      <h1 className="text-2xl font-karla"></h1>
+    </div>
+  );
 };
 
 export default page;
