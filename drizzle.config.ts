ff4fcb92dd8aa0d327e060cc5d57483370718fcf
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-13 22:58:07 +0545

    docs: betterAuth

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({ path: "./.env" }); // Load the Envs from the .env file
+
+export default defineConfig({
+  schema: "./drizzle/schema.ts", // Schema Path
+  out: "./drizzle/migrations", // Output Migration Path
+  dialect: "postgresql", // Database Type
+  dbCredentials: {
+    url: process.env.DATABASE_URL_POSTGRES!, // Config loads with dotenv packagae
+  },
+});
