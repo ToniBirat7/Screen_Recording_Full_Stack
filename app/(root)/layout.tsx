6f59cbef61611239f4d3171f05e3b92e0e5ad3ed
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-17 21:42:15 +0545

    fixed: auth checks in server component instead of middleware due to Edge Runtime issues

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import NavBar from "@/components/NavBar";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const layout = async ({ children }: { children: React.ReactNode }) => {
+  // Check for Authenticated Session to Access Dashboard, Else Redirect to Sign-in
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  // Check if session exists
+  if (!session) {
+    redirect("/sign-in");
+  }
+
+  // Else continue normal flow
   return (
     <div>
       <NavBar />
