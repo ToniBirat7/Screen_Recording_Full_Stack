14580f0381f48406431c8833bb56f7723a56093d
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-19 23:00:17 +0545

    docs: sign in with google fix

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL_POSTGRES,
 });
 
 export const db = drizzle(pool); // Export the DB connection
