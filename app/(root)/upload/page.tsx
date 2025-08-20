cbd522b099fbe3c8c82300284d1fdf5b5c3ef036
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-20 22:06:48 +0545

    docs: upload video cont

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -5,15 +5,22 @@ import FormField from "@/components/FormField";
 import FileInput from "@/components/FileInput";
 
 const page = () => {
-  const [error, setError] = useState(); // For error while uploading video
+  const [error, setError] = useState(null); // For error while uploading video
 
   return (
     <div className="wrapper-md upload-page">
       <h1>Upload a Video</h1>
+
+      {/* Check for Error if Error is True, then Display the Error */}
+
       {error && <div className="error-field">{error}</div>}
 
-      <FormField></FormField>
-      <FileInput></FileInput>
+      {/* Create the Form to Handle Video Upload */}
+
+      <form className="rounded-20 shadow-10 gap-6 w-full flex flex-col px-5 py-7.5">
+        <FormField></FormField>
+        <FileInput></FileInput>
+      </form>
     </div>
   );
 };
