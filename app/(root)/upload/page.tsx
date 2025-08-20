1010125e7b50e7d3cf1dae2fdd9998accc1fa3e9
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-20 21:57:31 +0545

    docs: upload video cont

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import FormField from "@/components/FormField";
+import FileInput from "@/components/FileInput";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="wrapper page">
+      <FormField></FormField>
+      <FileInput></FileInput>
+    </div>
+  );
 };
 
 export default page;
