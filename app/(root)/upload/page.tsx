8d77384a647aa991b236fab435b4485e231c8c2a
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-20 22:01:38 +0545

    docs: upload video cont

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import FormField from "@/components/FormField";
 import FileInput from "@/components/FileInput";
 
 const page = () => {
+  const [error, setError] = useState(); // For error while uploading video
+
   return (
-    <div className="wrapper page">
+    <div className="wrapper-md upload-page">
+      <h1>Upload a Video</h1>
+      {error && <div className="error-field">{error}</div>}
+
       <FormField></FormField>
       <FileInput></FileInput>
     </div>
