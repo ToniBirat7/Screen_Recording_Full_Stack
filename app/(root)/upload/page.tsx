b5889f8822ba2d92f3e03502269b8697b4592d79
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-20 22:17:46 +0545

    docs: upload video cont

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { ChangeEvent, use, useState } from "react";
 import FormField from "@/components/FormField";
 import FileInput from "@/components/FileInput";
+import { visibilities } from "@/constants";
 
 const page = () => {
   const [error, setError] = useState(null); // For error while uploading video
+
+  // State for form data
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    visibility: "public",
+  });
+
+  // Event Handler for Change in Form Data
+  const handleInputChange = (e: ChangeEvent) => {
+    const { name, value } = e.target; // Name is the name of the element and value will be changed value
+
+    // Change the previous data with the latest changed data
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+  };
 
   return (
     <div className="wrapper-md upload-page">
@@ -18,8 +34,14 @@ const page = () => {
       {/* Create the Form to Handle Video Upload */}
 
       <form className="rounded-20 shadow-10 gap-6 w-full flex flex-col px-5 py-7.5">
-        <FormField></FormField>
-        <FileInput></FileInput>
+        <FormField
+          id="Title"
+          label="Title"
+          placeholder="Enter a clear and concise video title"
+          value={}
+          onChange={}
+        />
+        <FileInput />
       </form>
     </div>
   );
