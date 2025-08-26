5297a664b2ec8d891a2e048632ad47f8381a17de
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-26 21:52:38 +0545

    docs: server action

--- lib/action/video.ts
+++ lib/action/video.ts
@@ -2,6 +2,7 @@
 
 import { withErrorHandling } from "../utils";
 
+// Wrap with withErrorHandling function to avoid error
 export const getVideoUploadURL = withErrorHandling(async () => {
-  
+
 });
