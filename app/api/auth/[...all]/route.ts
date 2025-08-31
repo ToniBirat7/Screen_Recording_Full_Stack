a04b9ed508dc28a6140a2bdbd1911fda8626006a
ToniBirat7 <gotofreelancing1@gmail.com>
2025-08-31 18:21:21 +0545

    docs: arcjet implementation

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -1,6 +1,9 @@
 import { auth } from "@/lib/auth";
-import { slidingWindow, validateEmail } from "@arcjet/next";
+import { ArcjetDecision, slidingWindow, validateEmail } from "@arcjet/next";
 import { toNextJsHandler } from "better-auth/next-js";
+import { NextRequest } from "next/server";
+import ip from "@arcjet/ip";
+import aj from "@/arcjet";
 
 // Email Validation -> Arcject Email Protection Spam Email, Temp Email Sites
 const emailValidation = aj.withRule(
@@ -19,5 +22,31 @@ const rateLimit = aj.withRule(
     characteristics: ["fingerprint"],
   })
 );
+
+//
+const protectAuth = async (req: NextRequest): Promise<ArcjetDecision> => {
+  const session = await auth.api.getSession({ headers: req.headers });
+  let userId: string;
+
+  // Set Session if exists else Use IP Address of the User
+  if (session?.user?.id) {
+    userId = session.user.id;
+  } else {
+    userId = ip(req) || "127.0.0.1"; // Or Default if not IP is Found
+  }
+
+  // If the user is trying for Sign-in
+  if (req.nextUrl.pathname.startsWith("/api/auth/sign-in")) {
+    const body = await req.clone().json(); // Get the body of the Request
+
+    if (typeof body.Email === "string") {
+      // Check if it is String
+      return emailValidation.protect(req, { email: body.email }); // Validate the Email
+    }
+  }
+
+  // Return
+  return rateLimit.protect(req, { fingerprint: userId });
+};
 
 export const { GET, POST } = toNextJsHandler(auth.handler);
