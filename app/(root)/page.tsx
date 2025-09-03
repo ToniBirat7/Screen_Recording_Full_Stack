7da818c2043ed09d44de253c0413f3a6c01148c6
ToniBirat7 <gotofreelancing1@gmail.com>
2025-09-03 17:21:33 +0545

    docs: video metadata fetch with pagination

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -28,7 +28,7 @@ const Page = async ({ searchParams }: SearchParams) => {
         videos?.length > 0 ? (
           <section className="video-grid">
             {videos.map((_, item) => (
-              <p>{video.}</p>
+              <p>{item}</p>
             ))}
           </section>
         ) : (
