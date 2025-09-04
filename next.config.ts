34166f6d2474bd4b4b021abab63bb72df8c46aeb
ToniBirat7 <gotofreelancing1@gmail.com>
2025-09-04 22:45:52 +0545

    error: invalid src prop

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "snapcast.b-cdn.net",
+        port: "",
+        pathname: "/**", // allow all paths
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**", // Google OAuth profile pics
+      },
+    ],
+  },
 };
 
 export default nextConfig;
