f2d1ab8459a8f20c5fed937deb46d9cb38ccbb5e
ToniBirat7 <gotofreelancing1@gmail.com>
2025-09-06 18:35:05 +0545

    docs: video details page

--- app/(root)/video/[id]/page.tsx
+++ app/(root)/video/[id]/page.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import VideoPlayer from "@/components/VideoPlayer";
+import { getVideoById } from "@/lib/action/video";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const page = () => {
+const page = async ({ params }: Params) => {
+  // Get the promise from params
+  const { id } = await params;
+  console.log("Video Id", id);
+
+  // get videoId and UserId
+  const { user, video } = await getVideoById(id);
+
+  console.log("Func", await getVideoById(id));
+
+  console.log("Video", video);
+  console.log("User", video);
+
+  // check for video
+  if (!video) redirect("/404");
+
   return (
-    <main className='wrapper page'>
-      Video Page
+    <main className="wrapper page">
+      <VideoPlayer videoId={id}></VideoPlayer>
     </main>
-  )
-}
+  );
+};
 
 export default page;
---
+++ components/VideoPlayer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoPlayer = ({ videoId }: { videoId: string }) => {
+  return <div>VideoPlayer</div>;
+};
+
+export default VideoPlayer;
