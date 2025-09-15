74fb2a877aac8a910b1b9385a8e7a1c5fabd9954
ToniBirat7 <gotofreelancing1@gmail.com>
2025-09-15 18:47:54 +0545

    docs: rec cont..

--- app/(root)/upload/page.tsx
+++ app/(root)/upload/page.tsx
@@ -43,6 +43,23 @@ const page = () => {
         const { url, name, type, duration } = JSON.parse(blobUrl);
         const blob = await fetch(url).then((res) => res.blob());
         const file = new File([blob], name, { type, lastModified: Date.now() });
+
+        if (video.inputRef.current) {
+          const dataTransfer = new DataTransfer();
+          dataTransfer.items.add(file);
+          video.inputRef.current.files = dataTransfer.files;
+
+          const event = new Event("change", { bubbles: true });
+          video.inputRef.current.dispatchEvent(event);
+          video.handleFileChange({
+            target: { files: dataTransfer.files },
+          } as ChangeEvent<HTMLInputElement>);
+        }
+
+        if (duration) setVideoDuration(duration);
+
+        sessionStorage.removeItem("recordedVideoURL");
+        URL.revokeObjectURL(url);
       } catch (e) {
         console.error(e, "Error loading recorded video");
       }
